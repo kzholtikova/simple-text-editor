99eb310e4108331a3110d951d439c92257206fe4
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-05-23 00:49:40 +0300

    new line command
    - reading text via buffer
    - handling empty list

---
+++ src/text_commands.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include "../include/text_commands.h"
+
+char* readText() {
+    size_t bufferSize = 100; // Initial buffer size
+    char *pBuffer = (char*)malloc(bufferSize * sizeof(char));
+    char c;
+
+    printf("Enter the text: ");
+    while ((c = getchar()) != '\n') {
+        if (strlen(pBuffer) == bufferSize - 1) { // Reallocate memory if needed
+            bufferSize *= 2;
+            pBuffer = (char*)realloc(pBuffer, bufferSize * sizeof(char));
+        }
+        pBuffer[strlen(pBuffer)] = c; // Append the character to the buffer
+    }
+
+    pBuffer[strlen(pBuffer)] = '\0'; // Add the null terminator
+
+    return pBuffer;
+}
+
+void newLine(LinkedList* pContent) {
+    char *pText = readText();
+    // Handle an empty list
+    if (pContent->head == nullptr) {
+        pContent->head = createLine(pText);
+        pContent->tail = pContent->head;
+        return;
+    }
+
+    pContent->tail->next = createLine(pText);
+    pContent->tail = pContent->tail->next;
+    free(pText);
+}
+
