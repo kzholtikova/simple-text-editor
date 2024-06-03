063d8342d27c1aa1f26e24f776e9b60327ddb56b
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-06-03 22:54:53 +0300

    text storage
    - Line, LinkedList structs turned into classes

--- src/text_storage.cpp
+++ src/text_storage.cpp
@@ -1,17 +1,31 @@
-#include <cstdlib>
-#include <cstring>
+#include <iostream>
 #include "../include/text_storage.h"
 
-Line* createLine(char* text) {
-    Line *pLine = (Line*)malloc(sizeof(Line));  // Allocate memory to store 2 pointers
-    if (pLine == nullptr)
-        return nullptr;
-
-    if (text != nullptr) {
-        pLine->text = strdup(text);  // Allocate memory for the 'str' copy, copies, returns a pointer
-        pLine->next = nullptr;  // Initialize memory
+Line::Line(char* userText) {
+    if (userText != nullptr) {
+        text = strdup(text);  // Allocate memory for the 'str' copy, copies, returns a pointer
+        next = nullptr;  // Initialize memory
     } else
-        pLine->text = nullptr;
-
-    return pLine;
+        text = nullptr;
 }
+
+Line::~Line() {
+    if (text != nullptr)
+        free(text);  // Deallocate memory
+}
+
+LinkedList::LinkedList() {
+    head = nullptr;
+    tail = nullptr;
+    length = 0;
+}
+
+LinkedList::~LinkedList() {
+    Line* current = head;
+    while (current != nullptr) {
+        Line* next = current->next;
+        delete current;
+        current = next;
+    }
+}
+
