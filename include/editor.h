27728a1636cca879f4ce1cabbda519a5cbd48cfa
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-06-09 18:59:07 +0300

    text storage c++ refactoring

--- include/editor.h
+++ include/editor.h
@@ -6,10 +6,9 @@ public:
     static void newLine(LinkedList* content, const char* text);
     static void printText(LinkedList* content);
     static void appendText(LinkedList* content, const char* text);
-    static void insertBy(LinkedList* content, int lineIndex, int charIndex, const char* newText);
     static void search(LinkedList* content, const char* pattern);
+    static void insertBy(LinkedList* content, int lineIndex, int charIndex, const char* newText);
     static void replaceBy(LinkedList* content, int lineIndex, int charIndex, const char* newText);
     static void deleteBy(LinkedList* content, int lineIndex, int charIndex, size_t length);
-private:
     static Line* setPosition(LinkedList* content, int lineIndex, int charIndex);
 };
